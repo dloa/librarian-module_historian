51155c5305d09a62ecd614f1cac8b7522bf9cf1e
arvind-clarion <arvind.vyas@clariontechnologies.co.in>
2015-06-23 18:02:10 +0530

    Remove unused code

--- app/controllers/hr_data_points_controller.rb
+++ app/controllers/hr_data_points_controller.rb
@@ -1,5 +1,6 @@
 class HrDataPointsController < ApplicationController
   before_action :set_hr_data_point, only: [:show, :edit, :update, :destroy]
+  respond_to :html, :js
 
   # GET /hr_data_points
   # GET /hr_data_points.json
@@ -25,29 +26,20 @@ class HrDataPointsController < ApplicationController
   # POST /hr_data_points.json
   def create
     @hr_data_point = HrDataPoint.new(hr_data_point_params)
-
-    respond_to do |format|
-      if @hr_data_point.save
-        format.html { redirect_to @hr_data_point, notice: 'Hr data point was successfully created.' }
-        format.json { render :show, status: :created, location: @hr_data_point }
-      else
-        format.html { render :new }
-        format.json { render json: @hr_data_point.errors, status: :unprocessable_entity }
-      end
+    if @hr_data_point.save
+      redirect_to @hr_data_point, notice: 'Hr data point was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /hr_data_points/1
   # PATCH/PUT /hr_data_points/1.json
   def update
-    respond_to do |format|
-      if @hr_data_point.update(hr_data_point_params)
-        format.html { redirect_to @hr_data_point, notice: 'Hr data point was successfully updated.' }
-        format.json { render :show, status: :ok, location: @hr_data_point }
-      else
-        format.html { render :edit }
-        format.json { render json: @hr_data_point.errors, status: :unprocessable_entity }
-      end
+    if @hr_data_point.update(hr_data_point_params)
+      redirect_to @hr_data_point, notice: 'Hr data point was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -55,10 +47,7 @@ class HrDataPointsController < ApplicationController
   # DELETE /hr_data_points/1.json
   def destroy
     @hr_data_point.destroy
-    respond_to do |format|
-      format.html { redirect_to hr_data_points_url, notice: 'Hr data point was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to hr_data_points_url, notice: 'Hr data point was successfully destroyed.'
   end
 
   private
